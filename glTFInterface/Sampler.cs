59b46ecdc8fdfbb5445640b27b5a84c156afff4b
3DXScape <steve@opensiteplan.org>
2022-12-11 23:52:58 -0800

    Progressing
    Adding members

--- glTFInterface/Sampler.cs
+++ glTFInterface/Sampler.cs
@@ -6,52 +6,6 @@ using System.Threading.Tasks;
 
 namespace glTFInterface
 {
-    /*
-     * 
-
-
-
-
-wrapS
-
-integer
-
-S (U) wrapping mode.
-
-No, default: 10497
-
-wrapT
-
-integer
-
-T (V) wrapping mode.
-
-No, default: 10497
-
-name
-
-string
-
-The user-defined name of this object.
-
-No
-
-extensions
-
-extension
-
-JSON object with extension-specific objects.
-
-No
-
-extras
-
-extras
-
-Application-specific data.
-
-No     * 
-     */
     public class Sampler
     {
         // Type: integer
@@ -65,16 +19,31 @@ No     *
         //     9728 NEAREST, 9729 LINEAR, 9984 NEAREST_MIPMAP_NEAREST, 9985 LINEAR_MIPMAP_NEAREST, 9986 NEAREST_MIPMAP_LINEAR, 9987 LINEAR_MIPMAP_LINEAR
         public int minFilter { get; set; } = 9728;
 
-        // 
+        // Type: integer
+        // S(U) wrapping mode.
+        // Required: No, default: 10497
+        // Allowed values: 33071 CLAMP_TO_EDGE, 33648 MIRRORED_REPEAT, 10497 REPEAT
+        public int wrapS { get; set; } = 10497;
+
+        // Type: integer
+        // T(V) wrapping mode.
+        // Required: No, default: 10497
+        // Allowed values: 33071 CLAMP_TO_EDGE, 33648 MIRRORED_REPEAT, 10497 REPEAT
+        public int wrapT { get; set; } = 10497;
+
+        // Type: string
+        // The user-defined name of this object.
+        // Required: No
+        public string name { get; set; } = "not set";
 
         // Type: extension
         // JSON object with extension-specific objects.
         // Required: No
         public Extension[] extensions { get; set; } = new Extension[0];
+
         // Type: extras
         // Application-specific data.
         // Required: No
         public Extra[] extras { get; set; } = new Extra[0];
-
     }
 }
