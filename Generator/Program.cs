62552099333d5e5579a79caaba57d4f993ee846e
Eoin Mcloughlin <helloworld@eoinrul.es>
2025-09-02 11:14:36 +0100

    Add some simple argument handling, user info

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,33 +1,113 @@
 using KhronosGroup.Gltf.Generator.JsonSchema;
 
+using System.Collections.Generic;
 using System.IO;
 
 namespace KhronosGroup.Gltf.Generator
 {
     class Program
     {
+        static string defaultSchemaPath = Path.Combine(
+            // glTF-CSharp-Loader\Generator\bin\Debug\net7.0
+            "..", "..", "..", "..", "..",
+            "glTF", "specification", "2.0", "schema", "glTF.schema.json");
+        static string defaultOutputPath = Path.Combine(
+            "..", "..", "..", "..", "glTFLoader", "Schema");
+
+        static void Usage(int exitCode)
+        {
+            var executableName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            System.Console.WriteLine($"Usage: {executableName} [options] [root schema file]");
+            System.Console.WriteLine("\t[options]");
+            System.Console.WriteLine("\t\t-h Display this help and exit");
+            System.Console.WriteLine("\t\t-search <directory> Search <directory> for schema file references");
+            System.Console.WriteLine($"\t\t-output <directory> Output to <directory> (default {defaultOutputPath})");
+            System.Console.WriteLine($"\t[root schema file] Schema file to parse (default {defaultSchemaPath})");
+            System.Environment.Exit(exitCode);
+        }
+
+        static void Error(string message)
+        {
+            System.Console.Error.WriteLine(message);
+            System.Environment.Exit(1);
+        }
+
         static void Main(string[] args)
         {
-            var schemaPath = Path.Combine(
-                // glTF-CSharp-Loader\Generator\bin\Debug\net7.0
-                "..", "..", "..", "..", "..",
-                "glTF", "specification", "2.0", "schema", "glTF.schema.json");
+            string rootSchemaPath = null;
+            string outputDirPath = null;
+            var searchDirs = new List<string>();
 
-            if (args.Length > 0)
+            for (int i = 0; i < args.Length; i++)
             {
-                schemaPath = args[args.Length - 1];
+                if (args[i] == "-h" || args[i] == "--help" || args[i] == "/?")
+                {
+                    Usage(0);
+                }
             }
 
-            if (!File.Exists(schemaPath))
-                throw new FileNotFoundException(
-                    $"Could not find '{Path.GetFileName(schemaPath)}' " +
-                    $"at '{Path.GetDirectoryName(schemaPath)}' " +
-                    $"(full path {Path.GetFullPath(Path.GetDirectoryName(schemaPath))}).");
-            var loader = new SchemaLoader(schemaPath);
-
-            for (int i = 0; i < args.Length - 1; i++)
+            for (int i = 0; i < args.Length; i++)
             {
-                loader.AppendSchemaSearchDirectory(args[i]);
+                if (args[i] == "-search")
+                {
+                    i++;
+                    if (args.Length > i && Path.Exists(args[i]) &&
+                        (File.GetAttributes(args[i]) & FileAttributes.Directory) != 0)
+                    {
+                        searchDirs.Add(args[i]);
+                    }
+                    else
+                    {
+                        Error("-search: Requires a directory");
+                    }
+                }
+                else if (args[i] == "-output")
+                {
+                    i++;
+                    if (args.Length > i && Path.Exists(args[i]) &&
+                        (File.GetAttributes(args[i]) & FileAttributes.Directory) != 0)
+                    {
+                        outputDirPath = args[i];
+                    }
+                    else
+                    {
+                        Error("-output: Requires a directory");
+                    }
+                 }
+                else
+                {
+                    if (File.Exists(args[i]) &&
+                        (File.GetAttributes(args[i]) & FileAttributes.Normal) != 0)
+                    {
+                        if (rootSchemaPath == null)
+                        {
+                            rootSchemaPath = args[i];
+                        }
+                        else
+                        {
+                            Error("Only one root schema can be specified");
+                        }
+                    }
+                    else
+                    {
+                        Error($"Unhandled argument: {args[i]}");
+                    }
+                }
+            }
+
+            rootSchemaPath = rootSchemaPath ?? defaultSchemaPath;
+            outputDirPath = outputDirPath ?? defaultOutputPath;
+
+            if (!File.Exists(rootSchemaPath))
+                throw new FileNotFoundException(
+                    $"Could not find '{Path.GetFileName(rootSchemaPath)}' " +
+                    $"at '{Path.GetDirectoryName(rootSchemaPath)}' " +
+                    $"(full path {Path.GetFullPath(Path.GetDirectoryName(rootSchemaPath))}).");
+            var loader = new SchemaLoader(rootSchemaPath);
+
+            for (int i = 0; i < searchDirs.Count; i++)
+            {
+                loader.AppendSchemaSearchDirectory(searchDirs[i]);
             }
 
             loader.ParseSchemas();
@@ -35,7 +115,6 @@ namespace KhronosGroup.Gltf.Generator
             loader.EvaluateInheritance();
             loader.PostProcessSchema();
             var generator = new CodeGenerator(loader.FileSchemas);
-            var outputDirPath = Path.Combine("..", "..", "..", "..", "glTFLoader", "Schema");
             generator.CSharpCodeGen(Path.GetFullPath(outputDirPath));
         }
     }
